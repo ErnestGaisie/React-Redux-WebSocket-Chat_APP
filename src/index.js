1ebf36d05bb4b4db7301ad515f732b18a83bd40a
Ernest Gaisie <ernestyawgaisie@gmail.cm>
2022-08-16 07:15:41 +0000

    Create tests for adding message

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-
 import './index.css'
 import App from './App'
+import { addUser } from './actions'
 import chat from './reducers'
 
 const store = createStore(chat)
+
+// register ourselves as present in the chat
+store.dispatch(addUser('Me'))
 
 ReactDOM.render(
   <Provider store={store}>
